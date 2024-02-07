3f40d374363031558327d514688e827a6885064a
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-02-07 18:02:11 +0400

    first commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,4 @@
+print("subscribe to the channel:"  "https://www.youtube.com/@LowBudgetStories" )
+print('also subscribe this channel https://youtube.com/@Goal_Oriented_Academy__GOA?si=kvvpf9nwvYubQV2n' )
+print("მე დავუმეგობრდი goaს აკადემიიდან დათი ჩხიკვაძეს. დათი ჩემი შერჩეული ლიდერია")
+print('ჩემი აზრით goa ძალიან კაი აკადემიაა. აქ ყველა ადამიანი ახლოსაა წარმატებასთან მე მომეწონა ჩემი პირველი გაკვეთილი')
