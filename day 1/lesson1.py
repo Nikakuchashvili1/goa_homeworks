efb41b7522fcc3cf0a1cf3a8e6d99ba8b4381352
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-02-08 19:27:24 +0400

    goa_homework

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -1,1 +1,7 @@
-print("hello world")
+name = "Nika "
+surname = "Kuchashvili"
+age= 13
+height = 1.75
+is_ugly = False
+
+print ("my name is" + " " +name + " "+ surname + " i am " + str (age)+" Years old " + " and i am " + str(height)+ " in height" )
