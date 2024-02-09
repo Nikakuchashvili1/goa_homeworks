c3e843059fe9934069ea37914f54d10ffe5ff7a5
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-02-09 17:10:48 +0400

    second commit

--- day 2/lesson2.py
+++ day 2/lesson2.py
@@ -1,3 +1,7 @@
+
+
+
+
 print("subscribe to the channel:"  "https://www.youtube.com/@LowBudgetStories" )
 print('also subscribe this channel https://youtube.com/@Goal_Oriented_Academy__GOA?si=kvvpf9nwvYubQV2n' )
 print("მე დავუმეგობრდი goaს აკადემიიდან დათი ჩხიკვაძეს. დათი ჩემი შერჩეული ლიდერია")
