21d59a5c5b6af83d3385d8429956e15007a1aa3d
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-02-20 19:27:34 +0400

    second commit

---
+++ day 06/classwork .py
@@ -0,0 +1,3 @@
+user_name =input("what is your full name?:  ")
+user_age =input("what is you age?:  ")
+print ("you name is" + "user_name" + "and you age is" + user_age)
---
+++ day 06/lesson6 .py
@@ -0,0 +1,13 @@
+#user_neme = input("what is you name?:    ")
+#print("hallo" + user_name)
+
+
+
+
+user_name =input("what is you name?:   ")
+user_surname =input("what is you surname?:   ")
+user_age =input("what is you age?:   ")
+user_lives =input("where you live?:   ")
+
+
+print("you name is" +  user_name +"you surname is" + user_surname +"you age is" +user_age + "you live" + user_lives)
