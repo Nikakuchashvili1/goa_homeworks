4a5d448872edb07e2e6b926a64d6d86e47fe715c
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-02-24 22:52:45 +0400

    second commit

--- day 05/20 error .py
+++ day 05/20 error .py
@@ -1,11 +1,17 @@
-prin t(nika)
+print(nika)
+#აქ ჩვენ პრჭყალებში ეგრეთ წოდებულ string ფორმაში არ გვაქ ჩაწერილი nika
 pri nt (kuchashvili)
+#აქაც იგივე string ფორმაში არ გვაქ ჩაწერილი kuchashvili და print არასწორად წერია
 prin t "goa is best Academy")
-prop erty:
-seta ttr?
-pru nt ("I love goa Academy")
-memor yview
-lambd a(BaseExce ptionGroup)
-hasatt r)
-pri nt )goa i s on top 
-pr unrt
+#აქაც print_ი არასწორად წერია და ფრჩხილი აკლია
+prunt I love goa Academy
+#აქ ფრჩხილები არააქ და ბრჭყალები
+("goa in top"
+ #აქ print_ი არ წერია და ფრჩხილიერეთ აკლია
+ pri nt ("diko)
+ #აქაც print_ი არ წერია სწორად და ბრჭყალი აკლია 
+fro m t urtle impo rt 
+draw a home
+#აქ არასწორად წერია from turtle import * და კომენტარი არ ქონდა გაკეტებული draw a home_ს
+
+
