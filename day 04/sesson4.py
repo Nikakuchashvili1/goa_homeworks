0292a0d132b18bfe0be5e4238b74fac0cb0d431e
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-02-24 21:36:50 +0400

    second commit

---
+++ day 04/sesson4.py
@@ -0,0 +1,3 @@
+user_age = 13
+age_in_20_years = user_age +20
+print("you will be " + str(age_in_20_years) + "in 20 years")
--- day 06/lesson6 .py
+++ day 06/lesson6 .py
@@ -6,8 +6,19 @@
 
 user_name =input("what is you name?:   ")
 user_surname =input("what is you surname?:   ")
-user_age =input("what is you age?:   ")
+user_age =int(input("what is you age?:   "))
 user_lives =input("where you live?:   ")
 
 
-print("you name is" +  user_name +"you surname is" + user_surname +"you age is" +user_age + "you live" + user_lives)
+print(" your name is " +  user_name + " your surname is " + user_surname + " your age is " +str(user_age) + " you live " + user_lives)
+
+age_in_20_years = user_age +20
+print("you will be " + str(age_in_20_years) + "in 20 years")
+
+
+
+
+
+
+
+
