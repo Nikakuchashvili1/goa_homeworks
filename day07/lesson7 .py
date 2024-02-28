de9e166cfa35c3e7c002c86877a597361fd3fe92
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-02-28 19:06:48 +0400

    second commit

--- day07/lesson7 .py
+++ day07/lesson7 .py
@@ -1,6 +1,10 @@
+#დღევანდელი ნასწავლი მასალის გამოყენებით შექმენით რამოდენიმე სხვადასხვა ტიპის ცვლადი და დაპრინტეთ მათი მონაცემთა ტიპები ი
 car = "bmw"
 bmw_year = 2021
 bmw_model ="f90"
 print(type(car))
 print(type(bmw_year))
 print(type(bmw_model))
+
+
+
---
+++ day08/lesson8 .py
@@ -0,0 +1,8 @@
+#მომხმარებელს შეეკითხეთ მისი ასაკი, ასევე შეეკითხეთ რამდენი წლით სურს დროში მოგზაურობა და დაპრინტეთ რამდენი წლის იქნება დროში მოგზაურობის შემდეგ
+
+user_age =int(input("what is you age?:   "))
+future_in_50_years =  user_age +50
+
+print(" your age is " + str (user_age))
+
+print("your future" + str(future_in_50_years))
