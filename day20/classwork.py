07604f7f88dd389c3a169a453fd663865cb10877
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-04-09 13:55:42 +0400

    second commit

---
+++ day20/classwork.py
@@ -0,0 +1,17 @@
+
+# print("goa best")
+# print("goa best")
+# print("goa best")
+
+
+
+for i in range(501):
+    print (str(i) + "   I Love You ❤️")
+
+
+
+for i in range(20, 100, 2):
+    print(i)
+
+    for i in range(20, 100, 3):
+        print(i)
