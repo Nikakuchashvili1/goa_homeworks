e4a1e831d2a4425c84146775ac5ef13da0b2fee4
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-04-16 13:12:45 +0400

    second commit

---
+++ day22/classwork.py
@@ -0,0 +1,19 @@
+# i =2
+# # while 5 > i:
+# #     print(i)
+
+# #     i += 1
+
+
+# space = ""
+
+# for i range('50'):
+#     print(space,  i)
+#     space + =''
+
+
+queston = input(" რომელი აკადემია არის საუკეთესო საქათველოში (გოა თუ ნოვატორი)")
+if queston == "ნოვატორი":
+    print( "არაა სიმართლე ")
+elif queston ==  "გოა":
+    print(" აბა რა")    
