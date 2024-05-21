7777b5551fbdd4aab8133369233d7401d6338221
Nikakuchashvili1 <kuchashvilinika9@gmail.com>
2024-05-21 21:52:34 +0400

    second commit

---
+++ day32/classwork.py
@@ -0,0 +1,18 @@
+# result =""
+
+
+
+# for  i in range(11):
+#     result +=
+
+# final_str =" "
+# final_str = final_str + "g"
+# final_str += "g"
+# print( final_str)
+
+
+
+result = ''
+for i in range:
+    result += "goa is best "
+print(result)
---
+++ day32/homework.py
@@ -0,0 +1,7 @@
+# 3) დღევანდელი გაკვეთილის მსგავსად შემქენით ცვალდი "goa is the best" ფორ ციკლის მეშვეობით და დღეს ნასწავლი ინდექსაციით შეართეთ  "goa is the best" სფეისების გარეშე ❤️
+
+
+result = ''
+for i in range:
+    result += "goaisthebest"
+print(result)
